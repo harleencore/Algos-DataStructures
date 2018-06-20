43cc9260ae989eab3f039b5b08c488fa0714ea8e
harleencore <harleen.k.sidhu@gmail.com>
2018-06-20 23:43:34 +0800

    reversestring in 3 ways

--- reversestring/index.js
+++ reversestring/index.js
@@ -6,6 +6,41 @@
 //   reverse('hello') === 'olleh'
 //   reverse('Greetings!') === '!sgniteerG'
 
-function reverse(str) {}
+// stephen says to avoid this for loop!!!!
+function myReverse(str) {
+  ans = ""; // let!!!
+  for (i=str.length-1; i>=0; i--){ //var i!!!!
+    ans += str[i];
+  }
+  return ans;
+}
+
+function arrayReverse(str) {
+  // const arr = str.split('');
+  // arr.reverse();
+  // return arr.join('');
+  return str.split('').reverse().join('');
+
+}
+
+function forLoopReverse(str){
+  let reversed = '';
+
+  for (let character of str){
+    reversed = character + reversed;
+  }
+
+  return reversed;
+}
+
+function es5Reverse(str){
+  return str.split('').reduce((reversed, character) => {
+    return character + reversed;
+  }, '');
+}
+
+function reverse(str){
+  return str.split('').reduce((rev, char) => char + rev, '');
+}
 
 module.exports = reverse;
