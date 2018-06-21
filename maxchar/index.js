7b40aa404710faeacf2045f4b60e2ffc9c99d34d
harleencore <harleen.k.sidhu@gmail.com>
2018-06-21 21:04:49 +0800

    maxchar with map!

--- maxchar/index.js
+++ maxchar/index.js
@@ -5,6 +5,28 @@
 // maxChar("abcccccccd") === "c"
 // maxChar("apple 1231111") === "1"
 
-function maxChar(str) {}
+function maxChar(str) {
+  const chars = {};
+  let max = 0;
+  let maxChar = '';
+
+  for (let char of str) {
+    // if (!dict[char]){
+    //   dict[char] = 1;
+    // }
+    // else{
+    //   dict[char] ++;
+    // }
+    chars[char] = chars[char] + 1 || 1;
+  }
+  //object starts with o, but they didn't choose to use for of for obj
+  for (let char in chars){
+    if (chars[char] > max) {
+      max = chars[char];
+      maxChar = char;
+    }
+  }
+  return maxChar;
+}
 
 module.exports = maxChar;
