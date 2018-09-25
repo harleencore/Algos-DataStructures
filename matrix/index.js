1277a300d467fddf7f17ac8a2115179b1517d34c
harleencore <harleen.k.sidhu@gmail.com>
2018-09-25 13:38:09 +0800

    cleaning code!

--- matrix/index.js
+++ matrix/index.js
@@ -17,53 +17,39 @@
 
 
 
-// function matrix(n) {
-//   var bigMatrix = [];
-//   for( let row = 0; row< n; row++){
-//     bigMatrix.push([]);
-//   }
-//   for (let i=1; i<n^2; i++){
-//     if (bigMatrix[0].length < n){
-//       bigMatrix[0].push(i);
-//     }
-//     for (let j=1; j<n; j++ ){
-//       bigMatrix[j].push(i);
-//     }
-//     if (bigMatrix[n-1].length <n){
-//       bigMatrix[n-1].unshift(i);
-//     }
-//     for (let k=n-2; k>0; k--){
-//       bigMatrix[k].unshift(i)
-//     }
-//   }
-// }
-
 function matrix(n){
-  var results = [];
+
+  const results = [];
   for( let row = 0; row< n; row++){
     results.push([]);
   }
-  var counter = 1;
-  var startCol = 0;
-  var endCol = n-1;
-  var startRow = 0;
-  var endRow = n-1;
+
+  let counter = 1;
+  let startCol = 0;
+  let endCol = n-1;
+  let startRow = 0;
+  let endRow = n-1;
+
   while (startCol <= endCol && startRow <= endRow){
+    // Top row
     for (let col = startCol; col <= endCol; col++){
       results[startRow][col]= counter;
       counter++;
     }
     startRow++;
+    // Right row
     for (let row = startRow; row <= endRow; row++){
       results[row][endCol]= counter;
       counter++;
     }
     endCol--;
+    //Bottom row
     for (let col= endCol; col >= startCol; col--){
       results[endRow][col]= counter;
       counter++;
     }
     endRow--;
+    //Left row
     for (let row = endRow; row >= startRow; row--){
       results[row][startCol] = counter;
       counter++;
