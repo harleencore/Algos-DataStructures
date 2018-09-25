94dea2b100faf335654b1c7c426199c496a780b9
harleencore <harleen.k.sidhu@gmail.com>
2018-09-25 17:48:58 +0800

    adding runtime complexities

--- pyramid/index.js
+++ pyramid/index.js
@@ -58,7 +58,8 @@ function pyramid2ndAttempt(n) {
   }
 }
 
-//stephen's solution
+// stephen's solution
+// O(n^2) => quadratic time 
 function pyramidIterative(n){
   const midpoint = Math.floor((2 * n - 1) / 2);
   for (let row = 0; row < n; row ++){
