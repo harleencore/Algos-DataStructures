79cd93942f390138e7e4df31da72d5387fea26f4
harleencore <harleen.k.sidhu@gmail.com>
2018-09-26 01:22:13 +0800

    adding fib answers

--- fib/index.js
+++ fib/index.js
@@ -8,6 +8,47 @@
 // Example:
 //   fib(4) === 3
 
-function fib(n) {}
+// fib(15) takes approx 651ms
+// exponential time!
+// O(2^n)
+function fib(n) {
+  if (n < 2){
+    return n;
+  }
+
+  return fib(n-1) + fib(n-2);
+}
+
+// O(n)
+// fib(15) takes less than 1ms
+function fibIterative(n){
+  const fibArray = [];
+  for (let i = 0; i<=n; i++){
+    if (i<=1){
+      fibArray.push(i);
+    }
+    else{
+      fibArray.push(fibArray[i-1] + fibArray[i-2]);
+    }
+  }
+  return fibArray[n];
+}
+
+// memoisation
+fib = memoize(fib);
+
+function memoize(fn){
+  const cache = {};
+  // anonymous function
+  return function(...args){
+    if (cache[args]){
+      return cache[args];
+    }
+    const result = fn.apply(this, args);
+    cache[args] = result;
+
+    return result;
+  };
+}
 
 module.exports = fib;
