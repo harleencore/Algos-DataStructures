fbbf3b147fd669ff90309c50b3d47c36c3fedbed
harleencore <harleen.k.sidhu@gmail.com>
2018-09-26 21:43:21 +0800

    adding queue from stack

--- qfroms/index.js
+++ qfroms/index.js
@@ -14,6 +14,44 @@
 
 const Stack = require('./stack');
 
-class Queue {}
+class Queue {
+  constructor(){
+    this.mainStack = new Stack();
+    this.holderStack = new Stack();
+  }
+
+  remove(){
+    while(this.mainStack.peek()){
+      this.holderStack.push(this.mainStack.pop());
+    }
+    // mistake: don't return first!!!
+    // store, finish restoring original queue
+    // then return record.
+    // return this.holderStack.pop();
+    const record = this.holderStack.pop();
+    while(this.holderStack.peek()){
+      this.mainStack.push(this.holderStack.pop());
+    }
+    return record;
+  }
+
+  add(record){
+    this.mainStack.push(record);
+  }
+  peek(){
+    // var lastEl;
+    while(this.mainStack.peek()){
+      // my method peeks at every step
+      // stepehen's peeks ONCE, only when second stack is ready
+      // lastEl = this.mainStack.peek();
+      this.holderStack.push(this.mainStack.pop());
+    }
+    const record = this.holderStack.peek();
+    while(this.holderStack.peek()){
+      this.mainStack.push(this.holderStack.pop());
+    }
+    return record;
+  }
+}
 
 module.exports = Queue;
