6034e95118b09f8cf2e01495e62de1f4cd6ba7b5
harleencore <harleen.k.sidhu@gmail.com>
2018-10-03 15:54:52 +0800

    adding bubblesort

--- sorting/index.js
+++ sorting/index.js
@@ -2,6 +2,19 @@
 // Implement bubbleSort, selectionSort, and mergeSort
 
 function bubbleSort(arr) {
+  for (let i = 0; i< arr.length; i++){
+    // shrinking window because each step
+    // drags the largest element to the end
+    // so dont need to consider the end alr
+    for (let j = 0; j< arr.length -i; j++){
+      if  (arr[j] > arr[j+1]){
+        let holder = arr[j];
+        arr[j] = arr[j+1];
+        arr[j+1] = holder;
+      }
+    }
+  }
+  return arr;
 
 }
 
